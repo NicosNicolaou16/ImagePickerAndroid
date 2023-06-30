deb075814077fe1615c6b95de0bc042fff23bf2e
Nicos Nicolaou <nicosnicos16@gmail.com>
2023-06-30 18:24:47 +0300

    add compose method for video and camera

--- ImagePickerAndroid/src/main/java/com/nicos/imagepickerandroid/utils/image_helper_methods/ImagePickerCompose.kt
+++ ImagePickerAndroid/src/main/java/com/nicos/imagepickerandroid/utils/image_helper_methods/ImagePickerCompose.kt
@@ -1,16 +1,23 @@
 package com.nicos.imagepickerandroid.utils.image_helper_methods
 
+import android.content.Context
 import android.graphics.Bitmap
+import android.icu.text.SimpleDateFormat
 import android.net.Uri
 import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.FileProvider
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.File
+import java.util.Date
+import java.util.Objects
 
 private var imageHelperMethods = ImageHelperMethods()
 private var pickSingleImage: ManagedActivityResultLauncher<String, Uri?>? = null
@@ -19,6 +26,8 @@ private var pickMultipleImages: ManagedActivityResultLauncher<String, List<@JvmS
     null
 private var pickMultipleImagesWithBase64Values: ManagedActivityResultLauncher<String, List<@JvmSuppressWildcards Uri>>? =
     null
+private var takeCameraImage: ManagedActivityResultLauncher<Void?, Bitmap?>? = null
+private var takeCameraImageWithBase64Value: ManagedActivityResultLauncher<Void?, Bitmap?>? = null
 
 @Composable
 fun PickSingleImage(
@@ -198,4 +207,98 @@ fun PickMultipleImagesWithBase64Values(
 
 fun pickMultipleImagesWithBase64Value() {
     pickMultipleImagesWithBase64Values?.launch("image/*")
+}
+
+@Composable
+fun takeCameraImage(
+    scaleBitmapModel: ScaleBitmapModel?,
+    listener: (Bitmap?) -> Unit
+) {
+    val composableScope = rememberCoroutineScope()
+    takeCameraImage =
+        rememberLauncherForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
+            if (bitmap != null) {
+                if (scaleBitmapModel != null) {
+                    composableScope.launch(Dispatchers.Default) {
+                        imageHelperMethods.scaleBitmap(
+                            bitmap = bitmap,
+                            scaleBitmapModel = scaleBitmapModel
+                        ).collect { scaledBitmap ->
+                            composableScope.launch(Dispatchers.Main) {
+                                listener(scaledBitmap)
+                            }
+                        }
+                    }
+                } else {
+                    listener(bitmap)
+                }
+            }
+        }
+}
+
+fun takeCameraImage(context: Context) {
+    takeCameraImage?.launch(null)
+}
+
+@Composable
+fun takeCameraImageWithBase64Value(
+    scaleBitmapModel: ScaleBitmapModel?,
+    listener: (Bitmap?, String?) -> Unit
+) {
+    val composableScope = rememberCoroutineScope()
+    takeCameraImageWithBase64Value =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.TakePicturePreview()) { bitmap ->
+            if (bitmap != null) {
+                if (scaleBitmapModel != null) {
+                    composableScope.launch(Dispatchers.Default) {
+                        imageHelperMethods.scaleBitmap(
+                            bitmap = bitmap,
+                            scaleBitmapModel = scaleBitmapModel
+                        ).collect { scaledBitmap ->
+                            imageHelperMethods.convertBitmapToBase64(bitmap = bitmap)
+                                .collect { base64 ->
+                                    composableScope.launch(Dispatchers.Main) {
+                                        listener(scaledBitmap, base64)
+                                    }
+                                }
+                        }
+                    }
+                } else {
+                    composableScope.launch(Dispatchers.Default) {
+                        imageHelperMethods.convertBitmapToBase64(bitmap = bitmap)
+                            .collect { base64 ->
+                                composableScope.launch(Dispatchers.Main) {
+                                    listener(bitmap, base64)
+                                }
+                            }
+                    }
+                }
+            }
+        }
+}
+
+fun takeCameraImageWithBase64Value() {
+    takeCameraImageWithBase64Value?.launch(null)
+}
+
+@Composable
+fun PickVideo(
+    listener: (Bitmap?, Uri?) -> Unit
+) {
+    val context = LocalContext.current
+    pickSingleImage =
+        rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            var bitmap: Bitmap? = null
+            if (uri != null) {
+                bitmap = imageHelperMethods.convertUriToBitmap(
+                    contentResolver = context.contentResolver,
+                    uri = uri
+                )
+                listener(bitmap, uri)
+            }
+        }
+}
+
+fun pickVideo() {
+    pickSingleImage?.launch("video/*")
 }
