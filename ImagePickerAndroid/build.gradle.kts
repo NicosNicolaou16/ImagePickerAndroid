6b07668be156b1274327eb4b38424f7811453fd6
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-01-26 22:37:41 +0200

    update Jetpack Compose version, update Gradle version and update README file

--- ImagePickerAndroid/build.gradle.kts
+++ ImagePickerAndroid/build.gradle.kts
@@ -54,9 +54,9 @@ val multidexVersion by extra("2.0.1")
 val materialDesignVersion by extra("1.11.0")
 val recyclerViewVersion by extra("1.3.2")
 val composeCompilerVersion by extra("1.5.8")
-val composeVersion by extra("1.5.4")
-val composeFoundationVersion by extra("1.5.4")
-val composeMaterialVersion by extra("1.5.4")
+val composeVersion by extra("1.6.0")
+val composeFoundationVersion by extra("1.6.0")
+val composeMaterialVersion by extra("1.6.0")
 val composeMaterial3Version by extra("1.1.2")
 
 dependencies {
