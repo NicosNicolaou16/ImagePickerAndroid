3f683a87632f6a2187bcfe49534a3a6f69af1a5d
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-06-21 22:12:14 +0300

    update version

--- ImagePickerAndroid/build.gradle.kts
+++ ImagePickerAndroid/build.gradle.kts
@@ -98,7 +98,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.github.NicosNicolaou16"
                 artifactId = "ImagePickerAndroid"
-                version = "2.0.13"
+                version = "2.0.14"
                 from(components["release"])
             }
         }
