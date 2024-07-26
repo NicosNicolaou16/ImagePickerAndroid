d9adf8ff57c9bf49f72e26bba682538a8651f618
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-07-26 22:53:05 +0300

    update Jetpack Compose libraries and update other libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
 val appCompatVersion by extra("1.7.0")
 val coreKtxVersion by extra("1.13.1")
 val constraintLayoutVersion by extra("2.1.4")
-val lifeCycleAndLiveDataCompilerAndViewModelKTXVersion by extra("2.8.3")
+val lifeCycleAndLiveDataCompilerAndViewModelKTXVersion by extra("2.8.4")
 val activityVersion by extra("1.8.0")
 val fragmentVersion by extra("1.6.1")
 val coroutineVersion by extra("1.8.1")
