1dfeb13537075f8b011bcf3ac13dd4277580fce9
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-10-26 00:32:20 +0300

    update Kotlin version

--- ImagePickerAndroid/build.gradle.kts
+++ ImagePickerAndroid/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
     id("maven-publish")
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
 }
 
 android {
