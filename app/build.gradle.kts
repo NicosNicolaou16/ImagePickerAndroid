c9c602e672304bdc5996cacf097f2cb3ea6bae2e
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-06-27 01:29:55 +0300

    fix Kotlin version in Gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 
 kotlin {
     compilerOptions {
-        languageVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_3
+        languageVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_4
         jvmTarget = JvmTarget.fromTarget("17")
     }
 }
